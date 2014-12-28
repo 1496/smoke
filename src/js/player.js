51d43925e921457a89a499882cc91756fc400781
isiguro <isiguro@gmail.com>
2014-12-28 20:04:05 +0900

    add autoPlayMode

--- src/js/player.js
+++ src/js/player.js
@@ -7,6 +7,7 @@
     var pass = 'gif/';
     var image = "";
     var keyCodeArray = [81,87,69,82,84,89,85,73,79,80,65,83,68,70,71,72,74,75,76,90,88,67,86,66,78,77];
+    G.playMode = 'normal';
     
     G.init = function()
     {
@@ -22,12 +23,19 @@
     G.autoPlay = function()
     {
         var i = 0;
-        setInterval(changeGif,2000);
+        var time = 0;
+
+        if(G.playMode == 'auto')
+        {
+            setTimeout(changeGif,time);
+        }
 
 
 
         function changeGif()
         {
+            if(G.playMode == 'normal') return false;
+            time = 2000;
             var e = $.Event('keydown', { keyCode: keyCodeArray[i]});
             $(window).trigger(e);
 
@@ -39,6 +47,9 @@
             {
                 i++;
             }
+
+            setTimeout(changeGif,time);
+            
         };
     };
 
